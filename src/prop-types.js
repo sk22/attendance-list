e64c5c63ae0f4221ab5cf885c03f01ec89a76916
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-21 19:06:23 +0200

    Fix stuff

--- src/prop-types.js
+++ src/prop-types.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types'
 
 export const person = PropTypes.shape({ name: PropTypes.string.isRequired })
 export const people = PropTypes.arrayOf(person)
-export const status = PropTypes.oneOf([true, false, 'maybe'])
+export const status = PropTypes.oneOf(['yes', 'no', 'maybe'])
 export const list = PropTypes.shape({
   name: PropTypes.string.isRequired,
-  people: PropTypes.arrayOf(PropTypes.number).isRequired,
-  attendance: PropTypes.objectOf(status)
+  people: PropTypes.arrayOf(PropTypes.any).isRequired,
+  status: PropTypes.objectOf(status)
 })
