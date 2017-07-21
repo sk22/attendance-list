8b3cd56ba928dcf614d7ee557aba76d4be96b999
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-21 19:05:59 +0200

    Implement setStatus

--- src/actions.js
+++ src/actions.js
@@ -2,3 +2,4 @@ import { createAction } from 'redux-act'
 
 export const setPerson = createAction('set a person')
 export const setList = createAction('set a list')
+export const setStatus = createAction('set attendance status of person in list')
--- src/reducers/lists.js
+++ src/reducers/lists.js
@@ -1,15 +1,22 @@
 import { createReducer } from 'redux-act'
 
-import { setList } from '../actions'
+import { setList, setStatus } from '../actions'
 
 const initialState = {
   xxx: {
     name: 'Club Meeting',
     people: ['aaa', 'bbb'],
-    attendance: { aaa: true, bbb: 'maybe' }
+    status: { aaa: 'yes', bbb: 'maybe' }
   }
 }
 
-export default createReducer({
-  [setList]: (state, { id, list = {} }) => ({ ...state, [id]: list })
-}, initialState)
+export default createReducer(
+  {
+    [setList]: (state, { id, list = {} }) => ({ ...state, [id]: list }),
+    [setStatus]: (state, { person, status, id }) => ({
+      ...state,
+      [id]: { ...state[id], status: { ...state[id].status, [person]: status } }
+    })
+  },
+  initialState
+)
