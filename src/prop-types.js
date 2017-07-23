b0c9aaadc6f40ee9af4f4d835fbd03050ae4417f
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-23 20:12:23 +0200

    Adjust prop types

--- src/prop-types.js
+++ src/prop-types.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types'
 
 export const person = PropTypes.shape({ name: PropTypes.string.isRequired })
-export const people = PropTypes.arrayOf(person)
 export const status = PropTypes.oneOf(['yes', 'no', 'maybe'])
 export const list = PropTypes.shape({
   name: PropTypes.string.isRequired,
   people: PropTypes.arrayOf(PropTypes.any).isRequired,
   status: PropTypes.objectOf(status)
 })
+export const attendance = PropTypes.arrayOf(
+  PropTypes.shape({ id: PropTypes.any.isRequired, person, status })
+)
