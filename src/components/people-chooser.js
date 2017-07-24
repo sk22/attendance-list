939350ab54a2f974316f6ba390651ec8368c6a88
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-24 15:43:08 +0200

    Change code style

--- src/components/people-chooser.js
+++ src/components/people-chooser.js
@@ -4,35 +4,30 @@ import { person as personPropType } from '../prop-types'
 
 class PeopleChooser extends Component {
   onChoose = id => event => this.setState({ [id]: event.target.checked })
-  getTrue (state) {
-    return Object.keys(state).filter(key => Boolean(state[key]))
-  }
+  getTrue = state => Object.keys(state).filter(key => Boolean(state[key]))
 
   static propTypes = {
     onSubmit: PropTypes.func.isRequired,
     people: PropTypes.objectOf(personPropType).isRequired
   }
 
-  render () {
-    return (
-      <div>
-        <ul>
-          {Object.keys(this.props.people).map(id =>
-            <li>
-              {this.props.people[id].name}{' '}
-              <input type='checkbox' onChange={this.onChoose(id)} />
-            </li>
-          )}
-        </ul>
-        <button
-          type='button'
-          onClick={() => this.props.onSubmit(this.getTrue(this.state))}
-        >
-          Add
-        </button>
-      </div>
-    )
-  }
+  render = () =>
+    <div>
+      <ul>
+        {Object.keys(this.props.people).map(id =>
+          <li>
+            {this.props.people[id].name}{' '}
+            <input type='checkbox' onChange={this.onChoose(id)} />
+          </li>
+        )}
+      </ul>
+      <button
+        type='button'
+        onClick={() => this.props.onSubmit(this.getTrue(this.state))}
+      >
+        Add
+      </button>
+    </div>
 }
 
 export default PeopleChooser
