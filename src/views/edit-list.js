248456e31d274d33434c114ebb49f3261bb23c19
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-24 16:05:26 +0200

    Changes prop name to plural

--- src/views/edit-list.js
+++ src/views/edit-list.js
@@ -2,24 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import { setList } from '../actions'
+import { setList, addPeopleToList } from '../actions'
 import { list as listPropType, person as personPropType } from '../prop-types'
 import PeopleChooser from '../components/people-chooser'
 
-const EditList = ({ list, people, onNameChange, onPersonChoose }) => {
-  return (
-    <div>
-      <input type='text' value={list.name} onChange={onNameChange(list)} />
-      <PeopleChooser people={people} onChoose={onPersonChoose} />
-    </div>
-  )
-}
+const EditList = ({ list, people, onNameChange, onPeopleChoose }) =>
+  <div>
+    <input type='text' value={list.name} onChange={onNameChange(list)} />
+    <PeopleChooser people={people} onChoose={onPeopleChoose} />
+  </div>
 
 EditList.propTypes = {
   list: listPropType,
   people: PropTypes.objectOf(personPropType),
   onNameChange: PropTypes.func.isRequired,
-  onPersonChoose: PropTypes.func.isRequired
+  onPeopleChoose: PropTypes.func.isRequired
 }
 
 const mapStateToProps = ({ lists, people }, { match }) => ({
@@ -34,7 +31,9 @@ const mapDispatchToProps = (dispatch, { match }) => ({
         id: match.params.id,
         list: { ...list, name: event.target.value }
       })
-    )
+    ),
+  onPeopleChoose: people =>
+    dispatch(addPeopleToList({ id: match.params.id, people }))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditList)
